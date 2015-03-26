bd1e2259cc6fb3efb644560eaad5354d31c3f9e4
渔夫 <vzheng56@gmail.com>
2015-03-26 22:03:00 +0800

    添加地形和Transform理解

---
+++ Project/BaseUnity/Assets/Scrtips/ParentAndChildren.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParentAndChildren : MonoBehaviour {
+	public Transform parentTransform;
+	// Use this for initialization
+	void Start () {
+		//parentTransform.DetachChildren ();
+
+ 		int childCount = parentTransform.childCount;
+//
+		List<Transform> children = new List<Transform> ();
+
+		for (int i = 0; i<parentTransform.childCount; i++) {
+			children.Add(parentTransform.GetChild(i));
+		}
+
+		foreach (Transform child in children) {
+			child.parent =null;
+		}
+
+//		for (int i =0; i<childCount; i++) {
+//				children[i].parent = null;
+//		}
+
+//		for (int i =0; i<childCount; i++) {
+//			parentTransform.GetChild(0).parent = null;
+//		}
+
+	}
+
+	void Update(){
+		if (parentTransform) {
+			if(parentTransform.childCount>0) {
+				parentTransform.GetChild (0).parent = null;
+			}
+		}
+	}
+}
