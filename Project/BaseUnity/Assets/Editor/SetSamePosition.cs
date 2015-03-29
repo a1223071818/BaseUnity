ce8e2f8663a548bc9800f1b43325d93d11e82186
渔夫 <vzheng56@gmail.com>
2015-03-29 14:59:28 +0800

    添加脚本
    编辑器脚本——设置相同位置

---
+++ Project/BaseUnity/Assets/Editor/SetSamePosition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class SetSamePosition :ScriptableWizard {
+
+	public GameObject targetObject;
+	public GameObject movebleObject;
+
+	[MenuItem("MyTools/SetSamePosition")]
+	static void CreateWizard()
+	{
+		ScriptableWizard.DisplayWizard ("Set Same Position", typeof(SetSamePosition), "StartSet");
+	}
+
+	void OnWizardCreate()
+	{
+		Vector3 position = targetObject.GetComponent<Transform> ().position;
+		movebleObject.GetComponent<Transform> ().position = position;
+	}
+
+}
