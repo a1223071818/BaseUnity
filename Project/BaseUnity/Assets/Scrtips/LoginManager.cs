255516418f7a748d1cc551610a516be21666b44d
渔夫 <vzheng56@gmail.com>
2015-04-07 15:38:51 +0800

    add scene

---
+++ Project/BaseUnity/Assets/Scrtips/LoginManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour {
+	
+	public InputField  UserNameInputField;
+	public InputField  UserPasswordInputField;
+
+	private string userName = "yufu";
+	private string userPassword = "123456";
+
+	public void Login()
+	{
+		if (UserNameInputField.text == userName && UserPasswordInputField.text == userPassword) {
+			//load scene
+			Debug.Log ("Login Success");
+			Application.LoadLevel("PlayerScene");
+		} else {
+			Debug.Log ("Login Faild");
+		}
+
+	}
+}
