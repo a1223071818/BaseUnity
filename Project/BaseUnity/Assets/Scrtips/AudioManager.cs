81f07917cc2fd7395c6dddd4bebbb0f6c967c84d
渔夫 <vzheng56@gmail.com>
2015-04-21 20:24:25 +0800

    add mixer expose function
    添加AudioMixer 暴露参数功能

---
+++ Project/BaseUnity/Assets/Scrtips/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioMixer masterMixter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetMasterValuem(float volume){
+
+		if (masterMixter) {
+			masterMixter.SetFloat("MasterValuemParam",volume);
+		}
+	}
+}
